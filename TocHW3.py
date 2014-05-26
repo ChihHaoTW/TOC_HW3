72572fc3d6ad157f7a531cf89948f1f2b496a131
Mouther <za7788az@hotmail.com>
2014-05-26 15:55:56 +0800

    fix utf-8

--- TocHW3.py
+++ TocHW3.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import json
 
 jsonobject = json.load(file('5365dee31bc6e9d9463a0057'))
-print json.dumps(jsonobject, ensure_ascii=False)
+jsonobject = json.dumps(jsonobject, ensure_ascii=False)
+
+for jsonobjects in jsonobject:
+  print jsonobject["總價元"]
+
