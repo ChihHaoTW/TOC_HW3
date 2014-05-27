eebfa361d54c87b952ce9172673efeaea49d8694
Mouther <za7788az@hotmail.com>
2014-05-27 22:14:59 +0800

    almost done

--- TocHW3.java
+++ TocHW3.java
@@ -1,5 +1,3 @@
-//javac -cp org.json.jar TocHW3.java
-//java -cp .:org.json.jar TocHW3
 import org.json.*;
 import java.io.*;
 
@@ -18,41 +16,61 @@ public class TocHW3
 
     public static void main(String args[]) throws IOException
     {
-        //FileReader jsonfile = new FileReader("5365dee31bc6e9d9463a0057");
-        String s = "{\"persons\":" + "[{\"name\":\"张三\",\"age\":20},{\"name\":\"李四\",\"age\":21}]" + "}";
-        JSONObject jsonObj = new JSONObject(s);
-        //JSONArray ary = new JSONArray("[{\"name\":\"张三\",\"age\":20},{\"name\":\"李四\",\"age\":21}]");
         JSONArray ary = new JSONArray(read("5365dee31bc6e9d9463a0057"));
-        //System.out.println("姓名:"+result.getString("name")+" 年龄:"+result.getInt("age"));
-        //解析object形式
-        //s="{\"persons\":[\"张三\",\"李四\",\"王五\"]}";
-        jsonObj = new JSONObject(s);
-        //JSONArray jsonarr=jsonObj.getJSONArray("");
+
         long sum = 0;
         int length = 0;
         for(int i=0;i<ary.length();i++)
         {
             try
             {
-                if (ary.getJSONObject(i).getString("鄉鎮市區") != "文山區") 
+                if (!ary.getJSONObject(i).getString("鄉鎮市區").equals("大安區")) 
                     continue;
+                //System.out.println(ary.getJSONObject(i).getString("鄉鎮市區"));
             }
             catch(org.json.JSONException e1)
             {
+                System.out.println("鄉鎮市區 error");
                 continue;
             }
 
             try
             {
-                sum += ary.getJSONObject(i).getInt("總價元");
+                if (ary.getJSONObject(i).getString("土地區段位置或建物區門牌").indexOf("復興南路") == -1) 
+                    continue;
+                //System.out.println(ary.getJSONObject(i).getString("鄉鎮市區"));
             }
             catch(org.json.JSONException e2)
             {
+                System.out.println("土地區段位置或建物區門牌 error");
+                continue;
+            }
+
+            try
+            {
+                if (ary.getJSONObject(i).getInt("交易年月") < (103*100)) 
+                    continue;
+                //System.out.println(ary.getJSONObject(i).getString("鄉鎮市區"));
+            }
+            catch(org.json.JSONException e3)
+            {
+                System.out.println("交易年月 error");
+                continue;
+            }
+
+
+            try
+            {
+                sum += ary.getJSONObject(i).getInt("總價元");
+            }
+            catch(org.json.JSONException e4)
+            {
+                System.out.println("總價元 error");
                 length--;
             }
 
             length++;
-            //System.out.println(ary.getJSONObject(i));
+            //System.out.println(i);
         }
         System.out.println(sum / length); // when length = 0 ?
     }
