495b55f52ea0dc2d54e9fc012628ad726153e14d
Mouther <za7788az@hotmail.com>
2014-05-27 16:19:09 +0800

    fix array

--- TocHW3.java
+++ TocHW3.java
@@ -5,16 +5,15 @@ import org.json.*;
 public class TocHW3{
 
   public static void main(String args[]){
-    String s = "{\"person\":{\"name\":\"张三\",\"age\":20}}";
+    String s = "{\"persons\":" + "[{\"name\":\"张三\",\"age\":20},{\"name\":\"李四\",\"age\":21}]" + "}";
     JSONObject jsonObj = new JSONObject(s);
-    JSONObject  result = jsonObj.getJSONObject("person");
-    System.out.println("姓名:"+result.getString("name")+" 年龄:"+result.getInt("age"));
+    JSONArray ary = new JSONArray("[{\"name\":\"张三\",\"age\":20},{\"name\":\"李四\",\"age\":21}]");
+    //System.out.println("姓名:"+result.getString("name")+" 年龄:"+result.getInt("age"));
     //解析object形式
-    s="{\"persons\":[\"张三\",\"李四\",\"王五\"]}";
+    //s="{\"persons\":[\"张三\",\"李四\",\"王五\"]}";
     jsonObj = new JSONObject(s);
-    JSONArray jsonarr=jsonObj.getJSONArray("persons");
-    for(int i=0;i<jsonarr.length();i++){
-        System.out.println(jsonarr.getString(i));
-        }
+    //JSONArray jsonarr=jsonObj.getJSONArray("");
+    for(int i=0;i<ary.length();i++){
+        System.out.println(ary.getJSONArray(i).getJSONObject("name")); }
 }
 }
